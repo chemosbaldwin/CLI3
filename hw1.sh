dade57978a9901a2f5b59865a20794a993bbbdef
Ahmed Fayyad <32056458+chemosbaldwin@users.noreply.github.com>
2019-03-07 18:56:41 +0200

    Homework w0
    Module HTML/CSS_CLI week 0

---
+++ hw1.sh
@@ -0,0 +1,23 @@
+mkdir ./Ahmed
+cd Ahmed
+touch blank.txt
+
+for i in 1 2 3 4 5
+do
+  echo -e "Hello\n" >> greetings.txt
+done
+
+for e in 1 2 3 4 5
+do
+  cp greetings.txt $e.txt
+done
+
+echo "cat " >> pets.txt
+echo "dog " >> pets.txt
+echo "hamster " >> pets.txt
+
+echo "cat " >> commands.txt
+echo "ls " >> commands.txt
+echo "pwd " >> commands.txt
+
+cat pets.txt commands.txt | sort | uniq >> lovelyCommands.txt
